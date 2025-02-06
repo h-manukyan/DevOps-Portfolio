185676134f24d3ba65946584870d6dff7ecf4737
kali <kali@kali.kali>
2025-02-07 00:11:39 +0400

    Add system-monitor.sh script

--- system-monitor.sh
+++ system-monitor.sh
@@ -1,1 +1,7 @@
-echo "Hello, DevOps Portfolio!" > output.txt
+echo "System uptime and Active Services"
+echo "================================="
+# Display system uptime
+uptime
+echo ""
+# List active services
+systemctl list-units --type=service --state=running
